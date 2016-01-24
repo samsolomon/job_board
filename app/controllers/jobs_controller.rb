53eec6f481bb6f640cc47f9aeccaf712c53375a7
samsolomon <samuelrobertsolomon@gmail.com>
2016-01-24 11:49:14 -0500

    update nav links and alerts

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :find_job, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
     if params[:category].blank?
