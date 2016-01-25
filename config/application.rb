24a522f4a0fbb978a6897896279c6b51e3333c04
samsolomon <samuelrobertsolomon@gmail.com>
2016-01-24 19:37:06 -0500

    try to precompile rails admin

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module JobBoard
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
   end
 end
